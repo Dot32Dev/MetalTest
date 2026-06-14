8b659466434a33123a9592d166f0edc7f9921c60
Dot32 <61964090+Dot32Dev@users.noreply.github.com>
2026-06-15 01:26:26 +0800

    Initial Commit

---
+++ MetalTest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MetalTest
+//
+//  Created by Dot32  on 15/6/2026.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
