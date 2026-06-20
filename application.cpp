9a38c1de44641e048fe4f869fd30292ae9cee22b
Dot32 <61964090+Dot32Dev@users.noreply.github.com>
2026-06-20 20:15:00 +0800

    Delete comments, rotate square with arrow keys

--- application.cpp
+++ application.cpp
@@ -78,6 +78,13 @@ void Application::run() {
     while (!glfwWindowShouldClose(glfw_window)) {
         glfwPollEvents();
         
+        if (glfwGetKey(glfw_window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
+            model = glm::rotate(model, 0.01f, glm::vec3(0.0f, 0.0f, -1.0f));
+        }
+        if (glfwGetKey(glfw_window, GLFW_KEY_LEFT) == GLFW_PRESS) {
+            model = glm::rotate(model, -0.01f, glm::vec3(0.0f, 0.0f, -1.0f));
+        }
+        
         NS::AutoreleasePool* pool = NS::AutoreleasePool::alloc()->init();
         
         drawable = layer->nextDrawable();
@@ -85,8 +92,6 @@ void Application::run() {
         
         MTL::CommandBuffer* cmd = command_queue->commandBuffer();
         MTL::RenderCommandEncoder* enc = cmd->renderCommandEncoder(rpd);
-        
-        model = glm::rotate(model, 0.01f, glm::vec3(0.0f, 0.0f, 1.0f));
         
         enc->setRenderPipelineState(pipeline);
         enc->setVertexBuffer(vertex_buffer, 0, 0);
@@ -107,13 +112,6 @@ void Application::run() {
     }
 }
 
-//AppDelegate::~AppDelegate() {
-//    view->release();
-//    window->release();
-//    device->release();
-//    delete view_delegate;
-//}
-//
 NS::Menu* Application::createMenuBar() {
     NS::Menu* mainMenu = NS::Menu::alloc()->init();
     
@@ -184,72 +182,7 @@ NS::Menu* Application::createMenuBar() {
     
     return mainMenu->autorelease();
 }
-//
-//void AppDelegate::applicationWillFinishLaunching(
-//    NS::Notification* notification
-//) {
-//    NS::Menu* menu = createMenuBar();
-//    NS::Application* app = reinterpret_cast<NS::Application*>(
-//        notification->object()
-//    );
-//    app->setMainMenu(menu);
-//    app->setActivationPolicy(NS::ActivationPolicy::ActivationPolicyRegular);
-//}
-//
-//void AppDelegate::applicationDidFinishLaunching(
-//    NS::Notification* notification
-//) {
-//    CGRect frame = (CGRect){{100.0, 100.0}, {800.0, 600.0}};
-//
-//    window = NS::Window::alloc()->init(
-//        frame,
-//        NS::WindowStyleMaskClosable | NS::WindowStyleMaskTitled,
-//        NS::BackingStoreBuffered,
-//        false
-//    );
-//
-//    device = MTL::CreateSystemDefaultDevice();
-//
-//    view = MTK::View::alloc()->init(frame, device);
-//    view->setColorPixelFormat(MTL::PixelFormat::PixelFormatBGRA8Unorm_sRGB);
-//    view->setClearColor(MTL::ClearColor::Make(0.0, 0.0, 0.0, 1.0));
-//
-//    view_delegate = new ViewDelegate(device);
-//    view->setDelegate(view_delegate);
-//
-//    window->setContentView(view);
-//    window->setTitle(
-//        NS::String::string("MetalTest", NS::StringEncoding::UTF8StringEncoding)
-//    );
-//
-//    window->makeKeyAndOrderFront(nullptr);
-//
-//    NS::Application* app = reinterpret_cast< NS::Application* >(
-//        notification->object()
-//    );
-//    app->activateIgnoringOtherApps(true);
-//}
-//
-//bool AppDelegate::applicationShouldTerminateAfterLastWindowClosed(
-//    NS::Application* sender
-//) {
-//    return true;
-//}
-//
-//ViewDelegate::ViewDelegate(MTL::Device* device): 
-//    MTK::ViewDelegate(), 
-//    device(device->retain()),
-//    model(glm::mat4(1.0f))
-//{
-//    command_queue = device->newCommandQueue();
-//    buildQuad();
-//    pipeline = buildShader(
-//        "shaders/triangle.metal",
-//        "vertex_main",
-//        "frag_main"
-//    );
-//}
-//
+
 void Application::buildTriangle() {
     Vertex vertices[] = {
         {{-0.75, -0.75, 0.0}},
@@ -353,38 +286,3 @@ MTL::RenderPipelineState* Application::buildShader(
     
     return pipeline;
 }
-//
-//ViewDelegate::~ViewDelegate() {
-//    vertex_buffer->release();
-//    index_buffer->release();
-//    pipeline->release();
-//    command_queue->release();
-//    device->release();
-//}
-//
-//void ViewDelegate::drawInMTKView(MTK::View* view) {
-//    model = glm::rotate(model, 0.01f, glm::vec3(0.0f, 0.0f, 1.0f));
-//    
-//    NS::AutoreleasePool* pool = NS::AutoreleasePool::alloc()->init();
-//
-//    MTL::CommandBuffer* cmd = command_queue->commandBuffer();
-//    MTL::RenderPassDescriptor* rpd = view->currentRenderPassDescriptor();
-//    MTL::RenderCommandEncoder* enc = cmd->renderCommandEncoder( rpd );
-//    
-//    enc->setRenderPipelineState(pipeline);
-//    enc->setVertexBuffer(vertex_buffer, 0, 0);
-//    enc->setVertexBytes(glm::value_ptr(model), sizeof(simd::float4x4), 1);
-//    enc->drawIndexedPrimitives(MTL::PrimitiveTypeTriangle,
-//       NS::UInteger(6),
-//       MTL::IndexTypeUInt16,
-//       index_buffer,
-//       NS::UInteger(0),
-//       NS::UInteger(1)
-//   );
-//    
-//    enc->endEncoding();
-//    cmd->presentDrawable(view->currentDrawable());
-//    cmd->commit();
-//
-//    pool->release();
-//}
